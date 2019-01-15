863d1f981b98ec19927de23e0456446ab965163d
Tim C Giles Jr <timothy.giles.12@gmail.com>
2019-01-14 23:39:05 -0500

    add a static filler text function.

---
+++ react-site/src/helpers/filler.js
@@ -0,0 +1,25 @@
+import React from 'react';
+export const FillerText = () => {
+    return (
+
+        <section className="body-text">
+            <h2>
+                <a href="https://google.com">Here's a link to google for whatever reason</a>
+            </h2>
+            <p>
+            Lorem ipsum dolor amet church-key listicle taxidermy yr leggings subway tile migas thundercats. Poutine prism photo booth snackwave fingerstache schlitz, health goth etsy meggings tousled roof party cardigan tattooed. Shaman la croix beard roof party, tofu YOLO shoreditch microdosing mumblecore tote bag artisan sustainable whatever. Butcher thundercats chia everyday carry, pour-over single-origin coffee retro art party blue bottle tbh offal sustainable flexitarian.
+            </p>
+                <ul>
+                    <li>
+                    Palo santo cray pinterest, shabby chic coloring book kickstarter edison bulb literally fashion axe lomo narwhal brooklyn quinoa.
+                    </li>
+                    <li>
+                    Meh direct trade truffaut, ennui pabst bushwick banh mi intelligentsia shoreditch meditation adaptogen tattooed bicycle rights humblebrag food truck.   
+                    </li>
+                    <li>
+                    PBR&B glossier selfies flannel.   
+                    </li>
+                </ul>
+        </section>
+    );
+}

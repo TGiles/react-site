5f63cb0c165ffd29c36e334c7421933b36f77131
Tim C Giles Jr <timothy.giles.12@gmail.com>
2019-01-14 23:39:49 -0500

    add picture and component. Need to use responsive images generator.

---
+++ react-site/src/helpers/Profile-picture.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import './Profile-picture.css';
+
+export default class ProfilePicture extends Component {
+    render() {
+        return (
+            <img className="main-picture" src="/img/copy_copy_copy.jpg" alt="Tim Giles with a quizzative look on his face"/>
+        );
+    }
+}
